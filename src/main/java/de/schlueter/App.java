029c7b47c8b083934453dccc52042e576f08cb35
Maduki-tech <d.schlueter1011@gmail.com>
2024-03-25 20:43:07 +0100

    added command line interface

--- src/main/java/de/schlueter/App.java
+++ src/main/java/de/schlueter/App.java
@@ -1,13 +1,24 @@
 package de.schlueter;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+public class App implements Runnable {
+    @Option(names = {"-h", "--host"}, description = "Host to scan for") private String host;
+    @Option(names = {"-p", "--port"}, description = "Port to scan for") private String port;
+
+    @SuppressWarnings("deprecation")
+    public static void main(String[] args) {
+        CommandLine.run(new App(), args);
+    }
+
+    @Override
+    public void run() {
+	if (host != null) {
+	    System.out.println("Host: " + host);
+	}
+	if (port != null) {
+	    System.out.println("Port: " + port);
+	}
     }
 }
